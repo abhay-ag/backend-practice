ebcaa929b422d7a0b2753e236c5b6ad58d09cf89
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-01 21:28:34 +0530

    bug fixing started | debugging

--- login-signup/server/app.js
+++ login-signup/server/app.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const path = require('path')
 const app = express();
-const {data} = require('./users/package.json')
+const auth = require('./auth')
 
+// run the server from the home directory i.e backend-practice.
 app.use(express.static('./login-signup/public'))
+app.use(express.urlencoded({extended: false}))
 
-app.post('/login', (req, res) => {
-    
-})
+// using middleware
+app.post('/login',auth, (req, res) => {})
 
 app.listen(5000, () => {
     console.log('Server listening on port 5000...');
---
+++ login-signup/server/auth.js
@@ -0,0 +1,24 @@
+const {data} = require('./users/package.json')
+
+const auth = (req, res, next) => {
+    const {uName, pass} = req.body;
+    let flag = 0
+
+    for(let i = 0; i< data.length; i++){
+        if(data[i].uName === uName && data[i].pass === pass){
+            flag = 1;
+        }else if(data[i].uName === uName && data[i].pass !== pass){
+            flag = 2;
+        }
+    }
+
+    if(flag === 1){
+        return res.status(200).send(`Hello, ${uName}`)
+    }else if(flag === 2){
+        return res.status(401).send(`${uName}, you have entered wrong password!! Please try again`)
+    }
+
+    res.status(404).send(`User: ${uName} not found! Please try again.`)
+}
+
+module.exports = auth;
