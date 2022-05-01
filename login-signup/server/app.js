55a30eabb8b0cea333946f37305846b72569d51c
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-01 21:16:39 +0530

    middleware started | main js made

--- login-signup/server/app.js
+++ login-signup/server/app.js
@@ -1,4 +1,14 @@
 const express = require('express')
+const path = require('path')
 const app = express();
+const {data} = require('./users/package.json')
 
-app.use(express.static(''))
+app.use(express.static('./login-signup/public'))
+
+app.post('/login', (req, res) => {
+    
+})
+
+app.listen(5000, () => {
+    console.log('Server listening on port 5000...');
+})
